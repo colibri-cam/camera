fe9d377f4fbe4e67dab66f46376b15b2fd744911
Damir Batinović <damirados@gmail.com>
2025-10-25 18:27:12 +0200

    Initial commit

---
+++ lib/camera.ex
@@ -0,0 +1,18 @@
+defmodule Camera do
+  @moduledoc """
+  Documentation for `Camera`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Camera.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
